9c6d775b46dbfa4b4b691e94eaf64220ba56b7db
הדס בן עמרם <212365803@mbydomain>
2021-11-16 18:55:26 +0200

    Third

--- startup/src/main/java/com/example/springboot/textUtil.java
+++ startup/src/main/java/com/example/springboot/textUtil.java
@@ -6,6 +6,6 @@ public class textUtil{
    
     public String getText(){
         String[] arr = {"my","func","return","the","string"};
-        return StringUtils.abbreviate(sentence, 16);
+        return StringUtils.abbreviate(arr, 16);
     }
 }
